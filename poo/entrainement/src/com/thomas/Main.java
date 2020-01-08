543ba170fef1cc460b980a0dc56f17171c833cf6
thomas-ubuntu <thomas-pc-dell@saugarde>
2020-01-08 22:16:30 +0100

    un peu de menage

--- poo/entrainement/src/com/thomas/Main.java
+++ poo/entrainement/src/com/thomas/Main.java
@@ -5,12 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test");
-        String tab="bj sa va";
-        int x = Integer.parseInt("12");
-        System.out.println(x);
-        System.out.println(tab);
-        Scanner sc = new Scanner(System.in);
-        sc.
+        //System.out.println("test");
+        //String tab="bj sa va";
+        //int x = Integer.parseInt("12");
+        //System.out.println(x);
+        //System.out.println(tab);
+        //Scanner sc = new Scanner(System.in);
+        //sc.
+
+    int duree = 123;
+        int heure = duree/60;
+        int minute = duree-heure*60;
+        System.out.println("h="+heure+" m="+minute);
     }
 }
